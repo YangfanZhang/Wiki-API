603466d88a051e53365c5126cf7620f4fdb258a0
YangfanZhang <yf_chang@icloud.com>
2020-07-05 15:21:27 +1000

    post a new article

--- app.js
+++ app.js
@@ -26,6 +26,23 @@ app.get("/articles", function(req, res){
     })
 });
 
+app.post("/articles", function(req, res){
+
+    const newArticle = new Article({
+        title: req.body.title, 
+        content: req.body.content
+    });
+    newArticle.save(function(err){
+        if(!err){
+            res.send("success");
+        }
+        else{
+            res.semd(err);
+        }
+
+    });
+})
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
   });
