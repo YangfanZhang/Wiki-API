42a4ee895442fc99f04c77a57599e6f0d5d93d1d
YangfanZhang <yf_chang@icloud.com>
2020-07-05 15:50:25 +1000

    get a specific article

--- app.js
+++ app.js
@@ -50,6 +50,21 @@ app.route("/articles")
     })
 });
 
+
+//specific article
+app.route("/articles/:articleTitle")
+.get(function(req, res){
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+        if(foundArticle){
+            res.send(foundArticle);
+        }else {
+            res.send("No articles matching that title was found");
+        }
+    })
+});
+
+
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
   });
